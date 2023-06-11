67368383c80d4efa336e99f8e08e385a750dfd45
Ambrose Ling <lingambrose@gmail.com>
2023-06-11 16:31:41 -0400

    created linear regression model

---
+++ versioncheck.py
@@ -0,0 +1,20 @@
+# What version of Python do you have?
+import sys
+import tensorflow.keras
+import pandas as pd
+import sklearn as sk
+import tensorflow as tf
+import platform
+
+print("===============================================")
+print(f"Python Platform: {platform.platform()}")
+print(f"Tensor Flow Version: {tf.__version__}")
+print(f"Keras Version: {tensorflow.keras.__version__}")
+print()
+print(f"Python {sys.version}")
+print(f"Pandas {pd.__version__}")
+print(f"Scikit-Learn {sk.__version__}")
+gpu = len(tf.config.list_physical_devices('GPU'))>0
+print("GPU is", "available" if gpu else "NOT AVAILABLE")
+print(tf.config.list_physical_devices())
+
