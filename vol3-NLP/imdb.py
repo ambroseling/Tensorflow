5c32118ae09b66b50503b07909f36690d045380e
Ambrose Ling <lingambrose@gmail.com>
2023-06-22 19:03:14 -0400

    add more changes

--- vol3-NLP/imdb.py
+++ vol3-NLP/imdb.py
@@ -69,4 +69,51 @@ history = model.fit(x_train,y_train,
 
 
 #===============================================================================
-imdb_dir = 
+imdb_dir = "/Users/ambroseling/Desktop/Tensorflow/tensorflow-repo/Tensorflow/vol3-NLP/aclImdb"
+train_dir = os.path.join(imdb_dir,"train")
+
+labels = []
+texts = []
+
+for label_type in ['neg','pos']:
+    dir_name = os.path.join(train_dir,label_type)
+    for fname in os.listdir(dir_name):
+        if fname[-4:]=='.txt':
+            f = open(os.path.join(dir_name,fname))
+            texts.append(f.read())
+            f.close()
+            if label_type == 'neg':
+                labels.append(0)
+            else:
+                labels.append(1)
+maxlen = 100
+training_samples = 200
+validation_samples = 10000
+max_words = 10000
+
+tokenizer = Tokenizer(num_words = max_words)
+tokenizer.fit_on_texts(texts)
+sequences = tokenizer.texts_to_sequences(texts)
+
+print(f"Sequences shape: {type(sequences[0][0])}")
+word_index = tokenizer.word_index
+print(f"Word_index shape: {type(word_index)}")
+#print('Found %s unique tokens.'%len(word_index))
+data = tf.keras.preprocessing.sequence.pad_sequences(sequences,maxlen=maxlen)
+labels = np.asarray(labels)
+
+#[23,3,4531,435,231,54,31,5,...] => one sentence
+
+
+indices = np.arange(data.shape[0])
+np.random.shuffle(indices)
+data = data[indices]
+labels = labels[indices]
+
+x_train = data[:training_samples]
+y_train = labels[:training_samples]
+x_val = data[training_samples:training_samples+validation_samples]
+y_val = labels[training_samples:training_samples+validation_samples]
+
+
+
